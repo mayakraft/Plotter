a5f61bac1584ea3cc7dd3dc4522b603808d60688
robbykraft <robbykraft@gmail.com>
2016-04-02 00:00:41 -0400

    lang wave tilt

--- spiral-phyllotaxic.c
+++ spiral-phyllotaxic.c
@@ -39,15 +39,19 @@ int main(int argc, char **argv){
 	fprintf(file, "<g>\n");
 	fprintf(file, "<polyline fill=\"none\" stroke=\"#000000\" stroke-miterlimit=\"10\" points=\"");  // hanging open quote
 
+
 	float x, y;
-	float SCALE = 26;
-	float ENDCAP = .4;
-	float ROUNDS = 11;//15.0;//18.0;
-	
 	float divider = 5 * 60;
 
+
+	float SCALE = 26;
+	float ROUNDS = 11;
 	float wobbleFreq = 19.0;
 	float wobbleMag = 0.4;
+	// float SCALE = 45;
+	// float ROUNDS = 7;
+	// float wobbleFreq = 7.0;
+	// float wobbleMag = 0.33;
 
 	for(int i = 0; i < ROUNDS; i+=2){
 		for(float a = 0; a < TWOPI; a += TWOPI/divider){
